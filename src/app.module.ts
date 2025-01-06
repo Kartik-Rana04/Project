026826936ad8c2762f96f59259054f5f3b59b4f7
darshil069shah <darshil069shah@gmail.com>
2025-01-06 15:42:39 +0530

    Db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/hero'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
